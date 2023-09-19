e91c3cf3636acd2095e62b153dd1b019a838be9b
Aryan7440 <aryanshukla744@gmail.com>
2023-09-19 16:06:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import AboutUs from './components/AboutUs'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Services from './components/Services'
+import Tours from './components/Tours'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {/* <!-- NAVBAR start --> */}
+      <Navbar></Navbar>
+      {/* <!-- NAVBAR END --> */}
+      {/* <!-- HOME START --> */}
+      <Home></Home>
+      {/* <!-- HOME END --> */}
+      {/* <!-- ABOUT US START --> */}
+      <AboutUs></AboutUs>
+      {/* <!-- ABOUT US END --> */}
+      {/* <!-- OUR SERVICES SECTION --> */}
+      <Services></Services>
+      {/* <! -- Tours-Section -->  */}
+      <Tours></Tours>
+      {/* <! -- Tours-Section-end -->  */}
+      {/* <!-- Foooter --> */}
+      <Footer></Footer>
+      {/* <!-- Foooter-END --> */}
+    </>
+  )
 }
 
-export default App;
+export default App
